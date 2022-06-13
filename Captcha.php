797f21ca81105b258fd87d35980bb002049973f1
saadben2020 <vierzonbs2020@gmail.com>
2022-06-13 12:06:10 +0200

    modifi echo

--- Captcha.php
+++ Captcha.php
@@ -6,8 +6,10 @@ $str = '<img style="margin:0px auto;width:500px;height:500px;" src="image/ok.png
 $str1 = '<img style="margin:0px auto;width:500px;height:500px;" src="image/no.png">';
 if ($user_question == $questiion) {
     echo $str;
+    echo '<h1>votre formulaire a bien été envoyer</h1>';
 } else {
     echo $str1; 
+    echo '<h1>votre formulaire n\'a pas été envoyer</h1>';
 }
 
 ?>
