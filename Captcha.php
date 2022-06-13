97eb8c7586d1e1aa5a7315c8da31ddad4a9e1bbd
saadben21 <88485505+saadben21@users.noreply.github.com>
2022-06-13 13:51:02 +0200

    Update Captcha.php

--- Captcha.php
+++ Captcha.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$questiion = $_SESSION["question"];
+$question = $_SESSION["question"];
 $user_question = $_POST["question"];
 $str = '<img style="margin:0px auto;width:500px;height:500px;" src="image/ok.png">';
 $str1 = '<img style="margin:0px auto;width:500px;height:500px;" src="image/no.png">';
